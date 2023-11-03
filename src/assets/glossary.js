1b7b7a68c5e6a68fcab102d5db62ecb14469945f
sabbamitta <43345786+sabbamitta@users.noreply.github.com>
2023-11-03 13:36:51 +0100

    add Site-TBA

--- src/assets/glossary.js
+++ src/assets/glossary.js
@@ -123,6 +123,15 @@ const Glossary = [{
     EBT-Vue3 is written using Vue3 and Vuetify3.
   `,
 },{
+  title: "Site-TBA",
+  summary: "French EBT-Site hosted by Noé Ismet",
+  link: "https://www.pannabijani.net/site-tba",
+  voice: "EBT-Site",
+  text: `
+    French language EBT-Site
+  `,
+  github: "https://github.com/pannabijani/site-tba",
+},{
   title: "pli.api.sc-voice.net",
   summary: "Pali (pli) Voice API server",
   link: "https://pli.api.sc-voice.net/status",
